a80461edf516371b028c6842a082683c36326421
Gabriel Henrique Cerruti de Sousa <66427525+CerrutiGH@users.noreply.github.com>
2021-12-06 11:25:04 -0300

    aulas 6/12

--- Reist VS2017/App_Start/RouteConfig.cs
+++ Reist VS2017/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace Reist_VS2017
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "Reist_VS2017.Controllers" }
             );
         }
     }
---
+++ Reist VS2017/Areas/Funcionario/FuncionarioAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace Reist_VS2017.Areas.Funcionario
+{
+    public class FuncionarioAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Funcionario";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Funcionario_default",
+                "Funcionario/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "Reist_VS2017.Areas.Funcionario.Controllers" }
+            );
+        }
+    }
+}
